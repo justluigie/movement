f24e4b292ea721763ef73fec6c218381153c3578
Ralph Jebulan <ralph.jebulan@wiz.ai>
2022-07-26 14:05:20 +0800

    background particles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from "react";
 import './App.css';
+import './module/background'
+import Particles from 'react-tsparticles';
+import type { Engine } from "tsparticles-engine"
+import { loadFull } from 'tsparticles';
+import  particlesOptions  from './module/background.json';
+import { ISourceOptions } from 'tsparticles-engine';
+
 
 function App() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles options={particlesOptions as ISourceOptions} init={particlesInit}/>
+     <div className="container">
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"></img>
+                    <h3>Design</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"></img>
+                    <h3>Code</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"></img>
+                    <h3>Launch</h3>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>
+                        <a href="#">Read More</a>
+                </div>
+            </div>
+        </div>
     </div>
+    </div>
+    
   );
 }
 
